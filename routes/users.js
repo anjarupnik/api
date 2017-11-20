bd2467040e5a88ecd115d6991b7d4863ef43e0a2
anjarupnik <anjarupnik@hotmail.com>
2017-11-20 18:10:30 +0100

    create user

--- routes/users.js
+++ routes/users.js
@@ -2,14 +2,13 @@ const router = require('express').Router()
 const { User } = require('../server/models')
 
 router.post('/users', (req, res, next) => {
-  User.create(User.create({name: req.body.name, email: req.body.email}), req.body.password, (err, user) => {
-    if (err) {
-      err.status = 422
-      return next(err)
-    }
+  User.create({
+     name: req.body.name,
+     email: req.body.email
+    })
+       .then(user=> res.status(201).send(user))
+       .catch(error => res.status(400).send(error));
 
-    res.status(201).send(user)
-  })
 })
 
 module.exports = router
