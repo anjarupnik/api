64dab998742bc90e8e28f501f9bb52be7448ff4b
ErnstWevers <ernstwevers@gmail.com>
2017-11-21 14:10:58 +0100

    added framework for sending emails in legalmailer.js: needs to be moved to the relevant route later

---
+++ legalmailer.js
@@ -0,0 +1,26 @@
+var nodemailer = require('nodemailer');
+
+// create reusable transporter object using the default SMTP transport
+var transporter = nodemailer.createTransport({
+  service: 'gmail',
+  auth: {
+    user: 'legaljoemailer@gmail.com',
+    pass: 'currentpassword'
+  }
+});
+
+// setup e-mail data with unicode symbols
+const mailOptions = {
+    from: 'legaljoemailer@gmail.com', // sender address
+    to: 'legaljoemailer@gmail.com', // list of receivers
+    subject: 'Hello', // Subject line
+    text: 'Hello world ?', // plaintext body
+};
+
+// send mail with defined transport object
+transporter.sendMail(mailOptions, function(error, info){
+    if(err){
+        return console.log(err);
+    }
+    console.log('Message sent: ' + info.response);
+});
