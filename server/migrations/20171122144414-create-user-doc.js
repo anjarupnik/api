42b8ef946e6d392315cf08b54cb48301fc1e9259
ErnstWevers <ernstwevers@gmail.com>
2017-11-22 15:45:38 +0100

    replace File with userDoc for DB

--- server/migrations/20171122144414-create-user-doc.js
+++ server/migrations/20171122144414-create-user-doc.js
@@ -1,14 +1,14 @@
 'use strict';
 module.exports = {
   up: (queryInterface, Sequelize) => {
-    return queryInterface.createTable('Files', {
+    return queryInterface.createTable('userDocs', {
       id: {
         allowNull: false,
         autoIncrement: true,
         primaryKey: true,
         type: Sequelize.INTEGER
       },
-      gdFileId: {
+      gooDriDocId: {
         type: Sequelize.STRING
       },
       userId: {
@@ -28,6 +28,6 @@ module.exports = {
     });
   },
   down: (queryInterface, Sequelize) => {
-    return queryInterface.dropTable('Files');
+    return queryInterface.dropTable('userDocs');
   }
 };
